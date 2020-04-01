49eec182232dbfd919253ef751747831aa0745ce
kaias1jp <kaias1jp@gmail.com>
2020-04-01 12:32:23 +0900

    キャッシュを有効にしてみた

--- instanceticker-assist/instanceticker-json.php
+++ instanceticker-assist/instanceticker-json.php
@@ -1,5 +1,10 @@
 <?php
 include './instanceticker-json-function.php';
+$expires = 86400;
+header('Last-Modified: Fri Jan 01 2010 00:00:00 GMT');
+header('Expires: ' . gmdate('D, d M Y H:i:s T', time() + $expires));
+header('Cache-Control: private, max-age=' . $expires);
+header('Pragma: ');
 if (isset($_GET["mode"])) {
     $mode=$_GET["mode"];
     switch ($mode) {
