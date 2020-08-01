5e8ab1bac6fe9b36de68ffe453f649d65c87988b
kaias1jp <kaias1jp@gmail.com>
2020-08-01 18:26:20 +0900

    miy.pw tsv提供中止

--- instanceticker-assist/instanceticker-json.php
+++ instanceticker-assist/instanceticker-json.php
@@ -18,6 +18,6 @@ if (isset($_GET["mode"])) {
 } else {
     $mode="0";
 }
-$jsonresult = csvtojson("https://miy.pw/tsv/".$mode, "\t");
+$jsonresult = csvtojson("https://socialapi.app/tsv/".$mode, "\t");
 header('content-type: application/json; charset=utf-8');
 print $jsonresult;
