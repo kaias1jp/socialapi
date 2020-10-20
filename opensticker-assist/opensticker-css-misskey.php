edc97291677e291b9c7f04f49c7906c326cec084
Sekine Hidenori <kaias1jp@gmail.com>
2020-10-20 04:17:24 +0000

    default値をjsonから取得するように変更 	modified:   opensticker-css-misskey.php

--- opensticker-assist/opensticker-css-misskey.php
+++ opensticker-assist/opensticker-css-misskey.php
@@ -9,6 +9,7 @@ $jsonUrl = "https://socialapi.app/api/opensticker/json";
 $json = file_get_contents($jsonUrl);
 $obj = json_decode($json, true);
 $obj2 = $obj["data"];
+$default = $obj["default"];
 
 
 echo "@charset 'utf-8';\n";
@@ -16,6 +17,7 @@ echo " .body:before{font-size:12px!important;height:16px!important;animation:fad
 echo "display:block!important;white-space:pre!important;text-overflow:ellipsis!important;}\n";
 echo "@keyframes fade{0%{opacity:1;}1%{opacity:0.1;}96%,to{opacity:1;}}\n";
 foreach ($obj2 as $key => $val) {
+	$type = $val["type"];
 	if ($val['domain']==$mydomain) {
 		$a_title = ":not([title*='@'])";
 	} else {
@@ -23,35 +25,23 @@ foreach ($obj2 as $key => $val) {
 	}
 	echo " a[class*='eiwwqkts avatar']".$a_title ;
 	if ($val["fontColor"]=="") {
-		if ($val["type"]=="mastodon") {
-			$fontColor = "#fff";
-		} else if ($val["type"]=="misskey" || $val["type"]=="misskeylegacy") {
-			$fontColor = "#3c9";
-		} else if ($val["type"]=="pleroma") {
-			$fontColor = "#da5";
-		}
+		$fontColor = $default[$type]["fontColor"];
 	} else {
 		$fontColor = $val["fontColor"];
 	}
-    echo "+.main>.body:before{color:".$fontColor."!important;";
-    echo "padding-left:16px!important;";
+	echo "+.main>.body:before{color:".$fontColor."!important;";
+	echo "padding-left:16px!important;";
     
         
     
         echo "background:url('".$val["favicon"]."')";
-    if ($val["bgColor"]=="") {
-	    if ($val["type"]=="mastodon") {
-		    $bgColor = "#26a";
-	    } else if ($val["type"]=="misskey" || $val["type"]=="misskeylegacy") {
-		    $bgColor = "#444";
-	    } else if ($val["type"]=="pleroma") {
-		    $bgColor = "#123";
-	    }
-    } else {
-          $bgColor = $val["bgColor"][0];
-    }
-    echo ",linear-gradient(to left,transparent,".$bgColor." )!important; ";
-    echo "background-repeat:no-repeat,no-repeat!important;";
-    echo "background-size:auto 16px!important;";
-    echo "content:'" . $val["name"] . "'!important;}\n";
+	if ($val["bgColor"]=="") {
+		$bgColor = $default[$type]["bgColor"][0];
+	} else {
+		$bgColor = $val["bgColor"][0];
+	}
+	echo ",linear-gradient(to left,transparent,".$bgColor." )!important; ";
+	echo "background-repeat:no-repeat,no-repeat!important;";
+	echo "background-size:auto 16px!important;";
+	echo "content:'" . $val["name"] . "'!important;}\n";
 }
